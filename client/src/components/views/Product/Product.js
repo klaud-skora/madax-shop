42928b61f2f3f7bbe2a97a1650ed18571f6aa8d9
Klaudia Skóra <klaud.skora@gmail.com>
2020-05-13 23:00:43 +0200

    Finish product view

--- client/src/components/views/Product/Product.js
+++ client/src/components/views/Product/Product.js
@@ -6,6 +6,10 @@ import styles from './Product.module.scss';
 import { connect } from 'react-redux';
 import { getProductById } from '../../../redux/productsRedux';
 
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 class Component extends React.Component {
   static propTypes = {
     product: PropTypes.object,
@@ -15,9 +19,19 @@ class Component extends React.Component {
     const { product } = this.props;
     return (
       <div className={styles.root}>
-        <h2>{product.name}</h2>
-        <img src={`${process.env.PUBLIC_URL}/images/${product.image}`} alt={product.name} />
-        <p>Elo</p>
+
+        <div className={styles.productBox}>
+          <div className={styles.imageBox}>
+            <img src={`${process.env.PUBLIC_URL}/images/${product.image}`} alt={product.name} />
+          </div>
+          <div className={styles.orderBox}>
+            <h2>{product.name}</h2>
+            <p className={styles.price}>{product.price}zł <span className={styles.vat}> w tym VAT</span></p>
+            <TextField label="Ilość:" type="number" InputLabelProps={{ shrink: true }} defaultValue="1" className={styles.amount}/>
+            <Button to={process.env.PUBLIC_URL +'/'} className={styles.cart} >Dodaj do koszyka <ShoppingCartIcon /></Button>
+          </div>
+          <p>{product.description}</p>
+        </div>
       </div>
     );
   }
